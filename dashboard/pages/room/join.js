3ba532e9aca3e2149b214d746204b5d9b258e0af
eric <xsephiroth@foxmail.com>
2020-03-24 11:34:50 +0800

    Join page styles

--- dashboard/pages/room/join.js
+++ dashboard/pages/room/join.js
@@ -1,7 +1,12 @@
 import { useRef } from 'react';
 import { useRouter } from 'next/router';
+import classNames from 'classnames/bind';
 import Layout from '../../components/Layout';
 import { useJoinUser } from '../../hooks/webrtc';
+
+import styles from './room.module.scss';
+
+const cx = classNames.bind(styles);
 
 const Join = () => {
   const {
@@ -13,9 +18,16 @@ const Join = () => {
 
   return (
     <Layout>
-      <div>
-        <div>{roomId}</div>
-        <video ref={videoRef} playsInline autoPlay muted />
+      <div className={styles.Container}>
+        <div className={cx({ Box: true, Box__Playing: true })}>
+          <video
+            className={cx({ Video: true, Video__Playing: true })}
+            ref={videoRef}
+            playsInline
+            autoPlay
+            muted
+          />
+        </div>
       </div>
     </Layout>
   );
