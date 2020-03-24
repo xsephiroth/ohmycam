c7040ef1ebf05687fcbf41030e767f811498c424
eric <xsephiroth@foxmail.com>
2020-03-24 11:32:02 +0800

    Master page styles

--- dashboard/pages/room/master.js
+++ dashboard/pages/room/master.js
@@ -1,8 +1,13 @@
 import { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 import QRCode from 'qrcode';
+import classNames from 'classnames/bind';
 import Layout from '../../components/Layout';
 import { useMasterUser } from '../../hooks/webrtc';
+
+import styles from './room.module.scss';
+
+const cx = classNames.bind(styles);
 
 const Master = () => {
   const {
@@ -13,19 +18,56 @@ const Master = () => {
 
   useMasterUser(videoRef, roomId);
 
+  const [joinHref, setJoinHref] = useState('');
+  useEffect(() => {
+    setJoinHref(window.location.href.replace('master', 'join'));
+  }, []);
+
+  const handleRoomIdCopy = () => {
+    navigator.clipboard
+      .writeText(joinHref)
+      .then(() => console.log('copied'))
+      .catch(e => console.error(e));
+  };
+
   const [qrCode, setQRCode] = useState(null);
   useEffect(() => {
-    QRCode.toDataURL(window.location.href.replace('master', 'join'))
+    if (!joinHref) return;
+    QRCode.toDataURL(joinHref)
       .then(url => setQRCode(url))
       .catch(e => console.error('generate QRCode error', e));
-  }, []);
+  }, [joinHref]);
+
+  const [canplay, setCanplay] = useState(false);
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.addEventListener('canplay', () => {
+        setQRCode(null);
+        setCanplay(true);
+      });
+    }
+  }, [videoRef]);
 
   return (
     <Layout>
-      <div>
-        <div>{roomId}</div>
-        {qrCode && <img src={qrCode} alt="QRCode" />}
-        <video ref={videoRef} autoPlay playsInline />
+      <div className={styles.Container}>
+        <div className={cx({ Box: true, Box__Playing: canplay })}>
+          {qrCode && (
+            <div>
+              <img className={styles.QRCode} src={qrCode} alt="QRCode" />
+              <p className={styles.RoomId} onClick={handleRoomIdCopy}>
+                {roomId}
+                <i />
+              </p>
+            </div>
+          )}
+          <video
+            className={cx({ Video: true, Video__Playing: canplay })}
+            ref={videoRef}
+            autoPlay
+            playsInline
+          />
+        </div>
       </div>
     </Layout>
   );
