8f1d20ceb2fab19bbf405021a66e7ae5eaf809a7
eric <xsephiroth@foxmail.com>
2020-03-25 09:08:51 +0800

    Add some meta tag

--- dashboard/components/Layout/index.js
+++ dashboard/components/Layout/index.js
@@ -9,6 +9,7 @@ const Layout = ({ children }) => {
         <meta name="description" content="简单看 OhMyCam - 基于浏览器WebRTC的视频监控工具，让你在内网环境中快速搭建使用的视频监控工具。" />
         <meta name="keywords" content="视频监控,内网视频监控,手机视频监控,WebRTC,webrtc,WebRTC监控,camera,surveillance" />
         <meta name="author" content="Eric Song" />
+        <meta name="baidu-site-verification" content="tmkSSVwtrt" />
       </Head>
       {children}
     </>
